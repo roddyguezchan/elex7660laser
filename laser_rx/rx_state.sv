f1ebad9478e5344e73489965bd551ea6d6a3bff4
Rafael Banalan <alecbanalan@gmail.com>
2026-04-05 18:49:35 -0700

    Create rx_state.sv
    Module for sending out IR messages. Will be updated in the future for other messages.

---
+++ laser_rx/rx_state.sv
@@ -0,0 +1,27 @@
+module rx_state (
+	input logic clk50,
+	input logic reset,
+	input logic [11:0] q0, q1, q2, q3,
+	
+	output logic [23:0] dxdy,
+	output logic [1:0] status,
+	
+	output logic ir_out
+);
+	//logic en_ir;
+	logic on_target, micro;
+	logic [4:0] move_com;
+	
+	qpd_state qpd0 (.*);
+	
+	assign status = { on_target, micro };
+	
+	ir_tx ir0(
+		.clk50(clk50),
+		.reset(reset),
+		.start(on_target),
+		.data({ 3'b0, move_com }),
+		.ir_out(ir_out)
+	);
+
+endmodule
