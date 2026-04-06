70c3bad8c7092b099b8bc85c8ba7a1b590678456
Rafael Banalan <alecbanalan@gmail.com>
2026-04-05 20:08:24 -0700

    Update driver.sv

--- driver.sv
+++ driver.sv
@@ -1,13 +1,13 @@
 module driver
 (
 	input logic clk50,
-	input logic [3:0] ir_msg, // message from IR
-	output logic yaw_dir, pitch_dir
-	output logic yaw_step, pitch_step 
+	input logic yaw_step_en, pitch_step_en,
+	output logic yaw_dir, pitch_dir,
+	output logic yaw_step, pitch_step
 );
 
 	logic clk0;
-	clkdiv #(50_000_000, 10) c0 (clk, clk0);
+	clkdiv #(50_000_000, 1000) c0 (clk50, clk0);
 
 	always_ff @(posedge clk0) begin
 		yaw_step <= yaw_step_en ? !yaw_step : 1'b1;
