2bf1771e9d066e591777f1d1b224c351f4b6435a
roddyguezchan <rodriguezclc@me.com>
2026-04-06 18:27:38 -0700

    Cleaned up gitignore and removed generated artifacts

--- laser_rx/software/app/build/Debug/CMakeFiles/create-hex.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/create-hex"
-  "onchip_memory2_0.hex"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/create-hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- laser_rx/software/app/build/Debug/bsp/CMakeFiles/hal2_bsp.dir/HAL/src/alt_log_printf.c.obj.d
+++
@@ -1,2 +0,0 @@
-bsp/CMakeFiles/hal2_bsp.dir/HAL/src/alt_log_printf.c.obj: \
- C:/Users/rodri/OneDrive/Documents/BCIT/Level_6/ELEX7660/Labs/elex7660laser/laser_rx/software/bsp/HAL/src/alt_log_printf.c
